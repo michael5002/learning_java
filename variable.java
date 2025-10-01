13fe895e5eb5e5789285d7f63064c7866ebd3014
michael5002 <jolayemiolamide63@gmail.com>
2025-10-01 02:57:28 +0100

    variable.java

---
+++ variable.java
@@ -0,0 +1,71 @@
+#using jshell on windows cmd prompt
+jshell> int myValue = 5;
+myValue ==> 5
+
+jshell> System.out.print(myValue;)
+5
+jshell> Integer.MIN_VALUE
+$3 ==> -2147483648
+
+jshell> Integer.MAX_VALUE
+$4 ==> 2147483647
+
+jshell> System.out.print("minimum value =" + $3);
+minimum value =-2147483648
+jshell>
+
+
+jshell> System.out.print("maximum value =" + $4);
+maximum value =2147483647
+
+Byte
+jshell> Byte.MIN_VALUE
+$7 ==> -128
+
+jshell> Byte.MAX_VALUE
+$8 ==> 127
+
+jshell> System.out.print("Min value =" + $7);
+Min value =-128
+jshell> System.out.print("Max value =" + $8);
+Max value =127
+
+Short
+jshell> Short.MAX_VALUE
+$12 ==> 32767
+
+jshell> Short.MIN_VALUE
+$13 ==> -32768
+
+jshell> System.out.print("ShortMinValue =" + $13);
+ShortMinValue =-32768
+jshell> System.out.print("ShortMaxValue =" + $12);
+ShortMaxValue =32767
+
+Long
+jshell> Long.MIN_VALUE
+$16 ==> -9223372036854775808
+
+jshell> Long.MAX_VALUE
+$17 ==> 9223372036854775807
+
+jshell> System.out.print("Min value =" + $16);
+Min value =-9223372036854775808
+jshell>
+
+jshell> System.out.print("Max value =" + $17);
+Max value =9223372036854775807
+
+Float
+jshell> Float.MIN_VALUE
+$20 ==> 1.4E-45
+
+jshell> Float.MAX_VALUE
+$21 ==> 3.4028235E38
+
+jshell> System.out.print("Min vale =" + $20);
+Min value =1.4E-45
+jshell>
+
+jshell> System.out.print("Max vale =" + $21);
+Max value =3.4028235E38
